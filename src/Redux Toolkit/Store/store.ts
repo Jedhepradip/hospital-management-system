f7c2cc2ee0a054ed46815b6b89438a3b3228505c
pradip jedhe <pradipjedhe69@gmail.com>
2025-03-03 23:21:46 +0530

    all completed

--- src/Redux Toolkit/Store/store.ts
+++ src/Redux Toolkit/Store/store.ts
@@ -1,29 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import All_appointment from "../Features/All-appointment";
-import All_Doctors from "../Features/All-Doctors"
-import All_Facility from "../Features/All-Facility"
-import All_SpecialAppointment from "../Features/All-SpecialAppointment"
-import All_User from "../Features/All-User"
 import Blog from "../Features/Blog"
-import Personal_Appointment from "../Features/PersonalAppointment"
-import SpecialUser from "../Features/Special-Appointment"
 import User from "../Features/User"
 import gallery from "../Features/gallery"
-
+import All_User from "../Features/All-User"
+import All_Doctors from "../Features/All-Doctors"
+import All_Facility from "../Features/All-Facility"
+import SpecialUser from "../Features/Special-Appointment"
+import All_appointment from "../Features/All-appointment";
+import Personal_Appointment from "../Features/PersonalAppointment"
+import All_SpecialAppointment from "../Features/All-SpecialAppointment"
 
 export const store = configureStore({
     reducer: {
+        Blog: Blog,
+        User: User,
+        gallery: gallery,
+        AllUser: All_User,
+        AllDoctors: All_Doctors,
+        AllFacility: All_Facility,
         Allappointment: All_appointment,
-        AllDoctors : All_Doctors,
-        AllFacility : All_Facility,
-        AllSpecialAppointment : All_SpecialAppointment,
-        AllUser : All_User,
-        Blog :Blog,
-        PersonalAppointment : Personal_Appointment,
-        SpecialUserAppointment:SpecialUser,
-        User : User,
-        gallery:gallery,
+        SpecialUserAppointment: SpecialUser,
+        PersonalAppointment: Personal_Appointment,
+        AllSpecialAppointment: All_SpecialAppointment,
     },
 });
 
