3679c65a7310e32a2783520fdea5d115371640ad
pradip jedhe <pradipjedhe69@gmail.com>
2025-03-16 13:01:40 +0530

    all completed website

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,13 +3,13 @@ import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaYoutube } from "react
 export default function Footer() {
     return (
         <footer className="bg-slate-100 text-gray-800 py-10">
-            <div className="container mx-auto px-6">
+            <div className="container mx-auto px-3">
                 {/* Footer Top Section */}
-                <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-8 pb-8 border-b border-gray-400">
+                <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-4 pb-8 border-b border-gray-400">
                     {/* Department Section */}
-                    <div>
-                        <h2 className="text-lg font-semibold mb-3">Department</h2>
-                        <ul className="space-y-2 text-sm">
+                    <div >
+                        <h2 className="text-lg font-semibold mb-3 md:text-center">Department</h2>
+                        <ul className="space-y-2 text-sm md:text-center">
                             <li>Surgery</li>
                             <li>Women's Health</li>
                             <li>Radiology</li>
@@ -21,8 +21,8 @@ export default function Footer() {
 
                     {/* Company Section */}
                     <div>
-                        <h2 className="text-lg font-semibold mb-3">Company</h2>
-                        <ul className="space-y-2 text-sm">
+                        <h2 className="text-lg font-semibold mb-3 md:text-center">Company</h2>
+                        <ul className="space-y-2 text-sm md:text-center">
                             <li>Terms & Conditions</li>
                             <li>Privacy Policy</li>
                             <li>Company Support</li>
@@ -33,17 +33,17 @@ export default function Footer() {
 
                     {/* Contact Section */}
                     <div>
-                        <h2 className="text-lg font-semibold mb-3">Get in Touch</h2>
-                        <p className="text-sm">Support Available 24/7</p>
-                        <p className="text-sm">Email: support@email.com</p>
-                        <p className="text-sm">Mon - Fri: 08:30 - 18:00</p>
-                        <p className="text-sm">Phone: +23-456-6588</p>
+                        <h2 className="text-lg font-semibold mb-3 md:text-center">Get in Touch</h2>
+                        <p className="text-sm md:text-center">Support Available 24/7</p>
+                        <p className="text-sm md:text-center">Email: support@email.com</p>
+                        <p className="text-sm md:text-center">Mon - Fri: 08:30 - 18:00</p>
+                        <p className="text-sm md:text-center">Phone: +23-456-6588</p>
                     </div>
 
                     {/* Quick Links Section */}
                     <div>
-                        <h2 className="text-lg font-semibold mb-3">Quick Links</h2>
-                        <ul className="space-y-2 text-sm">
+                        <h2 className="text-lg font-semibold mb-3 md:text-center">Quick Links</h2>
+                        <ul className="space-y-2 text-sm md:text-center">
                             <li>About Us</li>
                             <li>Services</li>
                             <li>Careers</li>
@@ -54,7 +54,7 @@ export default function Footer() {
 
                     {/* Newsletter Subscription */}
                     <div>
-                        <h2 className="text-lg font-semibold mb-3">Subscribe</h2>
+                        <h2 className="text-lg font-semibold mb-3 md:text-center">Subscribe</h2>
                         <p className="text-sm mb-2">Stay updated with our latest news.</p>
                         <div className="flex">
                             <input
@@ -62,7 +62,7 @@ export default function Footer() {
                                 placeholder="Enter your email"
                                 className="px-3 py-2 w-full rounded-l-md border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             />
-                            <button className="px-4 py-2 bg-blue-600 text-white rounded-r-md hover:bg-blue-800">
+                            <button className="md:px-0 px-4 py-2 bg-blue-600 text-white rounded-r-md hover:bg-blue-800">
                                 Subscribe
                             </button>
                         </div>
--- src/Redux Toolkit/Features/All-SpecialAppointment.ts
+++ src/Redux Toolkit/Features/All-SpecialAppointment.ts
@@ -27,17 +27,9 @@ const initialState: SpecialAppointmentState = {
     AllSpecialAppointment: []
 };
 
-
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjY3YzY5MjExY2Q0ZTI0N2U5YjNjNjdiZCIsImVtYWlsIjoiUHJhZGlqZWRoZWRAZ2FpbC5jb20iLCJuYW1lIjoicHJhZGlwIn0.P2ovZ3fyS2Ml82puLqQbdVyg7EjY4F3iyVnG3izUosQ"
-
 export const DetchinAllSpecialAppointment = () => async (dispatch: AppDispatch) => {
     try {
-        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api-Specile/SpecileAppointments/user-appointments`, {
-            headers: {
-                // authorization: `Bearer ${localStorage.getItem("Token")}`
-                authorization: `Bearer ${token}`
-            }
-        });
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api-Specile/SpecileAppointments/user-appointments`, {});
         dispatch(SetSpecialAppointment(response.data));
     } catch (error) {
         console.error("Error fetching all appointments:", error);
